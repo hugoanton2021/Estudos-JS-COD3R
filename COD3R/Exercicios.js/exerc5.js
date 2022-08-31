2ed6ac18a77f7632ed7d475ad09d034f5d6d86e3
hugoanton2021 <109737579+hugoanton2021@users.noreply.github.com>
2022-08-31 19:44:16 -0300

    testecasa3108

---
+++ COD3R/Exercicios.js/exerc5.js
@@ -0,0 +1,5 @@
+function maiorIgual (num1, num2)
+{
+return console.log( num1 < num2 || num1 === num2)
+}
+maiorIgual(0, 0 )
--- COD3R/programsLesson's/Fundamentos/unarios.js
+++ COD3R/programsLesson's/Fundamentos/unarios.js
@@ -15,3 +15,40 @@ num2-- // ou --num2
 
 // só usar num++ e num--
 
+function contrariar (valor) 
+{
+  const tipo = typeof valor; //1
+
+  if (tipo === "boolean") //2
+  {
+    return !valor //3
+  }
+
+  else if  (tipo === "number") 
+  {
+    return - valor //4
+  }
+  else
+  {
+    return `Booleano ou Númerico esperado, mas o valor inserido foi ${tipo}` //5
+  }
+
+}
+
+console.log(contrariar(true))
+console.log(contrariar(3))
+console.log(contrariar("bb"))
+
+/**
+ * COISAS QUE NÃO SABIA OU NÃO LEMBRAVA
+ * 1- Para armazenar apenas o tipo de um valor em uma variável
+ * basta declarar a variável e o valor será "typeof x"
+ * 2-para comparar o valor de uma variável, devo chama-la e comparar (===) com o nome do tipo em""
+ * não sei o porquê, mas já vem com "" os tipos.
+ * 3-  para mostrar o valor/tipo contrario só colocar ! antes da var
+ * 4- para mostrar o valor negativado, colocar - antes da var 
+ * 5- ao mostrar  a var que dei o valor como sendo o tipo do numero que será digitado no console.log
+ * será impresso o tipo.
+ * 
+ */
+
