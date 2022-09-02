4ce3bf2a4850c42ab60a90cad1b8666db4cfc2fe
hugoanton2021 <109737579+hugoanton2021@users.noreply.github.com>
2022-09-01 22:41:17 -0300

    109casa

--- COD3R/Exercicios/exerc5.js
+++ COD3R/Exercicios/exerc5.js
@@ -3,3 +3,14 @@ function maiorIgual (num1, num2)
 return console.log( num1 < num2 || num1 === num2)
 }
 maiorIgual(0, 0 )
+
+
+
+const maiorIgualDois = (nu1, nu2) => {
+  return nu1 >= nu2 ;
+}
+
+
+console.log (maiorIgualDois( 0 , 0));
+
+
