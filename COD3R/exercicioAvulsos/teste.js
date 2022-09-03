d881b116404c38dfc267d29314354051dae5139f
hugoanton2021 <109737579+hugoanton2021@users.noreply.github.com>
2022-09-03 02:13:35 -0300

    03/09-casa

---
+++ COD3R/exercicioAvulsos/teste.js
@@ -0,0 +1,20 @@
+const teste = (num1, num2) =>console.log(`A soma é : ${num1+num2} e a subtração é: ${num1 - num2}`);
+teste(1,2);
+
+
+
+ 
+/**
+ * JEITO TRADICIONAL (EM OUTRAS LINGUAGENS)
+ * function teste (num1, num2) 
+ * {
+ *  console.log(`A soma é : ${num1+num2} e a subtração é: ${num1 - num2}`)
+ * }
+ * 
+ * ARROW FUNCTION COM MAIS DE UMA LINHA
+ * 
+ * const teste = (num1, num2) => {
+ * console.log(`A soma é : ${num1+num2} e a subtração é: ${num1 - num2}`)
+ * }
+ * 
+ */
